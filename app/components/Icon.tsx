a6f456a6cfbb3265ec3a42360c4425305a987563
Yury Feschenko <yu.feshenko@ekf.su>
2024-10-27 17:50:15 +0300

    feat: add clipboard icon

---
+++ app/components/Icon.tsx
@@ -0,0 +1,12 @@
+export default function Icon(props: { children: React.ReactElement; size?: number | string }) {
+  const { children, size } = props
+
+  return (
+    <span
+      className="inline-flex flex-none"
+      style={size ? { width: size, height: size } : undefined}
+    >
+      {children}
+    </span>
+  )
+}
